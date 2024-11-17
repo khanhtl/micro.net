4153f2e6fbe45956cf0a2daf389c7c130cfb5922
tlkhanh <longkhanhta@gmail.com>
2024-11-17 22:13:37 +0700

    First Microservice Catalog.API with Vertical Slice Architecture and CQRS

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+public interface IQuery<TResponse> : IRequest<TResponse>
+{
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price)
+    :ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // create product
+        var product = new Product
+        {
+            Name = request.Name,
+            Categories = request.Categories,
+            Description = request.Description,
+            ImageFile = request.ImageFile,
+            Price = request.Price,
+        };
+        // save to database
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
